627bb2c76f8a476a701f4c1d472615ba86783c20
silvannagc333 <sgranados10079@ufide.ac.cr>
2024-03-24 19:33:14 -0600

    carrito

---
+++ carrito.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+
+$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+$collection = $mongoClient->TiendaRopa->carrito;
+
+if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) > 0) {
+    echo "<ul>";
+    foreach ($_SESSION['carrito'] as $producto) {
+        echo "<li>{$producto['nombre']} - {$producto['precio']}</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>Su carrito de compras está vacío.</p>";
+}
+?>
